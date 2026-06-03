c2591761cd362dfa04bb8cad801db51296d706a3
Martin Schumann <martin.schumann@contao-garage.de>
2026-06-03 17:32:11 +0200

    Move .bash_history and .gitconfig to host-readable directory

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -12,10 +12,21 @@ if [[ "$(id -u)" = "0" ]]; then
     fi;
 
     socat TCP4-LISTEN:3306,bind=127.0.0.1,fork TCP4:172.16.1.12:3306 &
-fi;
 
-if [[ ! -f /home/ubuntu/vhosts ]]; then
-    sudo -u ubuntu ln -sr /srv/apache2/vhosts /home/ubuntu/
-fi
+    sudo -u ubuntu touch /home/ubuntu/.host-data/.gitconfig
+    sudo -u ubuntu touch /home/ubuntu/.host-data/.bash_history
+
+    if [[ ! -f /home/ubuntu/.gitconfig ]]; then
+        sudo -u ubuntu ln -sr /home/ubuntu/.host-data/.gitconfig /home/ubuntu/.gitconfig
+    fi
+
+    if [[ ! -f /home/ubuntu/.bash_history ]]; then
+        sudo -u ubuntu ln -sr /home/ubuntu/.host-data/.bash_history /home/ubuntu/.bash_history
+    fi
+
+    if [[ ! -f /home/ubuntu/vhosts ]]; then
+        sudo -u ubuntu ln -sr /srv/apache2/vhosts /home/ubuntu/
+    fi
+fi;
 
 exec "$@"
