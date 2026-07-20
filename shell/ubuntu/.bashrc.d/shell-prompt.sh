ebf60d7c1297e467ae96392a10efe6a2dc071dad
Martin Schumann <martin.schumann@contao-garage.de>
2026-07-20 13:55:52 +0200

    Add fallback for lacking git-sh-prompt

--- shell/ubuntu/.bashrc.d/shell-prompt.sh
+++ shell/ubuntu/.bashrc.d/shell-prompt.sh
@@ -1,1 +1,12 @@
-PS1="\[\033[01;31m\][\u@\H]\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\\$ "
+if [ -f /usr/lib/git-core/git-sh-prompt ]; then
+    . /usr/lib/git-core/git-sh-prompt
+
+    GIT_PS1_SHOWDIRTYSTATE=true
+    GIT_PS1_SHOWSTASHSTATE=true
+    GIT_PS1_SHOWUNTRACKEDFILES=true
+    GIT_PS1_SHOWCOLORHINTS=true
+
+    PROMPT_COMMAND='__git_ps1 "\[\033[01;31m\][\u@\H]\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" " \\$ "'
+else
+    PS1="\[\033[01;31m\][\u@\H]\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \\$ "
+fi
